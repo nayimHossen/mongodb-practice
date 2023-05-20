beb41366b80862a8ecc6d2d0e32caeb08a12f63c
Nayim Hossen <nayimh2015@gmail.com>
2023-05-21 05:18:44 +0600

    create a interface and connect with schema

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,73 @@
 import cors from "cors";
 
 import express, { Application, NextFunction, Request, Response } from "express";
+import { Schema } from "mongoose";
 
 const app: Application = express();
 
 //using cors
 app.use(cors());
 
+//parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World sultana!");
-  next();
+  //inserting a test data into mongodb
+  /*
+   * step1: Interface
+   * step2: Schema
+   * step3: Model
+   * step4: Database Query
+   */
+
+  //Creating a interface
+  interface IUser {
+    id: string;
+    role: "student";
+    password: string;
+    name: {
+      firstName: string;
+      middleName?: string;
+      lastName: string;
+    };
+    dateOfBirth?: string;
+    gender: "male" | "female";
+    email?: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    presentAddress: string;
+    permanentAddress: string;
+  }
+
+  //creating schema
+  const userSchema = new Schema<IUser>({
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: { type: String, required: true },
+    password: { type: String, required: true },
+    name: {
+      firstName: { type: String, required: true },
+      middleName: { type: String },
+      lastName: { type: String, required: true },
+    },
+    dateOfBirth: { type: String },
+    gender: {
+      gender: "male",
+      required: true,
+    },
+    email: { type: String },
+    contactNo: { type: String, required: true },
+    emergencyContactNo: { type: String, required: true },
+    presentAddress: { type: String, required: true },
+    permanentAddress: { type: String, required: true },
+  });
+
+  // res.send("Hello World sultana!");
+  // next();
 });
 
 export default app;
