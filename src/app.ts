cacc8ed7f9649bc6a03558b041f8e149b05efcf7
Nayim Hossen <nayimh2015@gmail.com>
2023-05-21 02:04:43 +0600

    connect with server

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+
+import express, { Application, NextFunction, Request, Response } from "express";
+
+const app: Application = express();
+
+//using cors
+app.use(cors());
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World sultana!");
+  next();
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+import app from "./app";
 
-const app = express();
-const port = 5000;
+const port: number = 5000;
 
 //database connection
-
 const databaseConnection = async () => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/practice");
     console.log(`Database connection successful :)`);
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
   } catch (error) {
     console.log(`Failed to connected`, error);
   }
 };
 
 databaseConnection();
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
